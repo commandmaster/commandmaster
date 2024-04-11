cce32b17fccdb1ada57d036529e60acd538c841b
Bennett <153877155+commandmaster@users.noreply.github.com>
2024-04-11 11:00:41 -0600

    ajusting resloution dynamically

--- demos/2dArray/sketch.js
+++ demos/2dArray/sketch.js
@@ -3,13 +3,13 @@ let grid1;
 const sketch = function(p5) {
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    grid1 = new Grid(p5, 5, 1000, 700);
-    p5.background(255);
+    grid1 = new Grid(p5, 1, 700, 700);
+    p5.background(0, 100, 100);
     p5.strokeWeight(2);
   }
 
   p5.draw = () => {
-    p5.background(0, 100, 100);
+    
     grid1.Update();
   }
 
@@ -29,6 +29,9 @@ class Grid{
     })
 
     this.#initGrid();
+    this.avgLoopTime = 0;
+    this.loopTimeSum = 0;
+    this.pastLoopTimes = [];
   }
 
   #initGrid(){
@@ -40,7 +43,7 @@ class Grid{
       this.grid[i] = [];
       for (let j = 0; j < cols; j++){
         const randColor = 255;
-        this.grid[i][j] = new GridPiece(randColor);
+        this.grid[i][j] = new GridPiece(i, j, randColor);
       }
     }
     
@@ -51,7 +54,7 @@ class Grid{
     this.p5.noStroke();
     for (let i = 0; i < this.grid.length; i++){
       for (let j = 0; j < this.grid[i].length; j++){
-        if (!this.grid[i][j].hasUpdated){
+        if (!this.grid[i][j].needUpdate){
           continue;
         }
         this.p5.fill(this.grid[i][j].color);
@@ -65,35 +68,38 @@ class Grid{
   #gridBehavior(){
     for (let i = 0; i < this.grid.length; i++){
       for (let j = 0; j < this.grid[i].length; j++){
+        
 
         if (!this.bgImg){
           continue;
         }
 
         let piece = this.grid[i][j];
+
         let pieceColor = this.p5.color(piece.color) ;
         let mouse = this.p5.createVector(this.p5.mouseX, this.p5.mouseY);
         let pieceVector = this.p5.createVector(j * this.resolution, i * this.resolution);
         let distance = this.p5.dist(mouse.x, mouse.y, pieceVector.x, pieceVector.y);
-
-        function lerpColor(p5, p5Color1, p5Color2, amount){
-          const red = p5.lerp(p5Color1.levels[0], p5Color2.levels[0], amount);
-          const green = p5.lerp(p5Color1.levels[1], p5Color2.levels[1], amount);
-          const blue = p5.lerp(p5Color1.levels[2], p5Color2.levels[2], amount);
-          return p5.color(red, green, blue);
-        }
-
-        // get pixel of bgImg associated with this grid piece
-        const realtiveX = j / this.grid[0].length;
-        const realtiveY = i / this.grid.length;
-        const relativeToImage = this.bgImg.get(realtiveX * this.bgImg.width, realtiveY * this.bgImg.height);
-        const pixelColor = this.p5.color(relativeToImage);
         
         const radius = 200;
         const brushStrength = 1;
         if (Math.abs(distance) < radius){
+          function lerpColor(p5, p5Color1, p5Color2, amount){
+            const red = p5.lerp(p5Color1.levels[0], p5Color2.levels[0], amount);
+            const green = p5.lerp(p5Color1.levels[1], p5Color2.levels[1], amount);
+            const blue = p5.lerp(p5Color1.levels[2], p5Color2.levels[2], amount);
+            return p5.color(red, green, blue);
+          }
+  
+          // get pixel of bgImg associated with this grid piece
+          const realtiveX = j / this.grid[0].length;
+          const realtiveY = i / this.grid.length;
+          const relativeToImage = this.bgImg.get(realtiveX * this.bgImg.width, realtiveY * this.bgImg.height);
+          const pixelColor = this.p5.color(relativeToImage);
+
+
           piece.color = lerpColor(this.p5, pieceColor, pixelColor, brushStrength);
-          piece.hasUpdated = true;
+          piece.needUpdate = true;
         } 
 
         
@@ -103,9 +109,58 @@ class Grid{
     
   }
 
+  #resetUpdates(){
+    for (let i = 0; i < this.grid.length; i++){
+      for (let j = 0; j < this.grid[i].length; j++){
+        this.grid[i][j].needUpdate = false;
+      }
+    }
+  }
+
   Update(){
+    //Dynamically adjust resolution based on average loop time
+    const targetLoopTime = 1000 / 60;
+    const loopTimeDifference = targetLoopTime - this.avgLoopTime;
+    const resolutionChange = loopTimeDifference / 100;
+    this.resolution += resolutionChange;
+    this.resolution = this.p5.constrain(this.resolution, 1, this.p5.max(this.pixelsX, this.pixelsY));
+    
+    //Change current grid to match new resolution
+    const grid = [];
+    const rows = this.pixelsY / this.resolution;
+    const cols = this.pixelsX / this.resolution;
+
+    for (let i = 0; i < rows; i++){
+      grid[i] = [];
+      for (let j = 0; j < cols; j++){
+        //get color of old grid piece but with new resolution
+        const oldI = Math.floor(i * (this.resolution / this.pixelsY));
+        const oldJ = Math.floor(j * (this.resolution / this.pixelsX));
+        const color = this.grid[oldI][oldJ].color;
+
+        grid[i][j] = new GridPiece(i, j, color);
+      }
+    }
+    this.grid = grid;
+
+
+    const startTime = this.p5.millis();
     this.#gridBehavior();
     this.#drawGrid();
+    this.#resetUpdates();
+    const endTime = this.p5.millis();
+    const loopTime = endTime - startTime;
+    this.pastLoopTimes.push(loopTime);
+    if (this.pastLoopTimes.length > 100){
+      this.loopTimeSum -= this.pastLoopTimes.shift();
+    }
+    this.loopTimeSum += loopTime;
+    this.avgLoopTime = this.loopTimeSum / this.pastLoopTimes.length;
+
+    
+    
+
+
   }
 
 }
@@ -114,9 +169,11 @@ class Grid{
 class GridPiece{
   color;
 
-  constructor(color){
+  constructor(i, j, color){
+    this.i = i;
+    this.j = j;
     this.color = color;
-    this.hasUpdated = true;
+    this.needUpdate = true;
   }
 }
 
